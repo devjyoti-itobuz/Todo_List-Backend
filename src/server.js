461d349546d078514d5471d1caac908d79ed286a
Devjyoti Banerjee <devjyoti@itobuz.com>
2025-10-28 17:20:21 +0530

    fix: wip

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ const PORT = config.PORT
 
 await connectDb()
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 app.use(loggerMiddleware)
 app.use('/user/auth', authRouter)
